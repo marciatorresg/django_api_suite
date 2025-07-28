045afe1698b1bc8837b1c11af4388dc41cac836e
Darvier88 <127569059+Darvier88@users.noreply.github.com>
2025-07-28 01:53:43 +0000

    Actu

--- demo_rest_api/urls.py
+++ demo_rest_api/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
    path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources" ),
    path("<str:item_id>/", views.DemoRestApiItem.as_view(), name="demo_rest_api_item"),
-
 ]
