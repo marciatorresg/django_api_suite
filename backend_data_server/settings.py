b13f6d2b3f992f608184c1390fa58c8ccbb63cef
marciatorresg <151395207+marciatorresg@users.noreply.github.com>
2025-07-28 15:51:01 +0000

    Guía 21: Django - DRF + Firebase Admin Python SDK

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+import firebase_admin
+from firebase_admin import credentials
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,8 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "firebase_admin",
     "rest_framework",
     "demo_rest_api",
+    "landing_api",
     "homepage",
     
 ]
@@ -129,3 +134,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CREDENTIALS_PATH = credentials.Certificate("secrets/landing-key.json")
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(FIREBASE_CREDENTIALS_PATH, {
+   'databaseURL': 'https://guia10-4d543-default-rtdb.firebaseio.com'
+})
