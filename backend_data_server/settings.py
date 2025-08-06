eb67c80c3bf344bfc71440a4741506863e8753ef
marciatorresg <151395207+marciatorresg@users.noreply.github.com>
2025-08-06 04:35:25 +0000

    carpeta assets

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-ozr-gm64u%(pe7hta1kx07+5*a124symx&o(e*ic31gz1!71fj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mtorresg.pythonanywhere.com']
 
 
 # Application definition
@@ -129,6 +129,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
 ]
+
+STATIC_ROOT = "assets/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
